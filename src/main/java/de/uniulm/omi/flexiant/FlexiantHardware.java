45ca1bfb7a2b33636fc8053d1e28c7ae55c824ce
Daniel Baur <daniel.baur@uni-ulm.de>
2014-12-05 14:47:18 +0100

    PS-29 #comment Test implementation of flexiant hardware

--- src/main/java/de/uniulm/omi/flexiant/FlexiantHardware.java
+++ src/main/java/de/uniulm/omi/flexiant/FlexiantHardware.java
@@ -2,8 +2,41 @@ package de.uniulm.omi.flexiant;
 
 import de.uniulm.omi.flexiant.extility.ProductOffer;
 
-public class FlexiantHardware extends AbstractFlexiantResource {
-    public FlexiantHardware(final ProductOffer productOffer) {
-        super(productOffer);
+public class FlexiantHardware implements FlexiantResource {
+
+    private final ProductOffer disk;
+    private final ProductOffer machine;
+
+    public FlexiantHardware(final ProductOffer disk, final ProductOffer machine) {
+        this.machine = machine;
+        this.disk = disk;
+    }
+
+    @Override
+    public String getId() {
+        return machine.getResourceUUID() + ":" + disk.getResourceUUID();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof FlexiantHardware && ((FlexiantHardware) obj).getId().equals(this.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String getName() {
+        return "GenericFlexiantHardware";
+    }
+
+    public int getCores() {
+        return 1;
+    }
+
+    public int getRam() {
+        return 1;
     }
 }
